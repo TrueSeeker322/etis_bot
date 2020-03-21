3750d8cfee6c64d4dc9d8cab02e5233788a498f7
True Seeker <murz.kirill@yandex.ru>
2020-03-21 23:40:16 +0500

    \fix\

--- setup.py
+++ setup.py
@@ -88,12 +88,12 @@ def bot_start(message):
     with closing(psycopg2.connect(DATABASE_URL, sslmode='require')) as conn:  # Обновление БД
         with conn.cursor() as cursor:
             cursor.execute("SELECT table_array, table_names FROM user_tables WHERE tg_id = %(tg_id)s",
-                           {'tg_id': message.from_user.id})
+                           {'tg_id': str(message.from_user.id)})
             if not cursor.fetchone():  # если в бд еще нет такой записи
-                cursor.execute("DELETE FROM user_tables WHERE tg_id = %(tg_id)s", {'tg_id': message.from_user.id})
+                cursor.execute("DELETE FROM user_tables WHERE tg_id = %(tg_id)s", {'tg_id': str(message.from_user.id)})
                 cursor.execute(
                     "INSERT INTO user_tables(tg_id,table_array,table_names) VALUES (%(tg_id)s,%(table_array)s,%table_names)s",
-                    {'tg_id': message.from_user.id, 'table_array': quarry_array, 'table_names': names_array})
+                    {'tg_id': str(message.from_user.id), 'table_array': quarry_array, 'table_names': names_array})
             else:  # если в бд есть такая запись, то проверим на сходство данных
                 print(cursor.fetchone()[0])
 
