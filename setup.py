464a6655d3836cb77cb2687d2f62c89256b4e5ca
True Seeker <murz.kirill@yandex.ru>
2020-03-21 23:43:42 +0500

    \fix\

--- setup.py
+++ setup.py
@@ -85,6 +85,8 @@ def bot_start(message):
     names_array = names_array[:len(names_array) - 1]
     names_array = names_array[:len(names_array) - 1]
     names_array += '}'
+    print(quarry_array)
+    print(names_array)
     with closing(psycopg2.connect(DATABASE_URL, sslmode='require')) as conn:  # Обновление БД
         with conn.cursor() as cursor:
             cursor.execute("SELECT table_array, table_names FROM user_tables WHERE tg_id = %(tg_id)s",
