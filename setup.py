a5965d7b762cc04449aa58a72b39bf4634950dfd
True Seeker <murz.kirill@yandex.ru>
2020-04-30 18:55:05 +0500

    \fix\

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-import telebot
 import os
-from flask import Flask, request
 import time
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-import random
+from etis import *
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
@@ -74,9 +72,49 @@ def login_handler(bot, update):
 
 def user_data_handler(bot, update):
     mes = update.message.reply_text(
-        'Логин: ' + login_dict.get(update.message.from_user.id) + '\n Пароль: ' + pass_dict.get(update.message.from_user.id))
+        'Логин: ' + login_dict.get(update.message.from_user.id) + '\n Пароль: ' + pass_dict.get(
+            update.message.from_user.id))
     time.sleep(6)
     bot.delete_message(chat_id=update.message.chat.id, message_id=mes.message_id)
+
+
+def auth_handler(bot, update):
+    if login_dict.get(update.message.from_user.id) == 'Не введён' or \
+            login_dict.get(update.message.from_user.id) is None or \
+            pass_dict.get(update.message.from_user.id) == 'Не введён' or \
+            pass_dict.get(update.message.from_user.id) is None:
+        bot.send_message(update.message.chat.id, 'Не ввёден логин или пароль. /login')
+    else:
+        auth_data = {'p_redirect'.encode('cp1251'): 'stu.timetable'.encode('cp1251'),
+                     'p_username'.encode('cp1251'): login_dict[update.message.from_user.id].encode('cp1251'),
+                     'p_password'.encode('cp1251'): pass_dict[update.message.from_user.id].encode('cp1251')}
+        session_dict[update.message.from_user.id] = requests.Session()  # добавление подключения в словарь
+        if authentication(auth_data, session_dict[update.message.from_user.id]):
+            with closing(psycopg2.connect(DATABASE_URL, sslmode='require')) as conn:  # Обновление БД
+                with conn.cursor() as cursor:
+                    cursor.execute("SELECT * FROM tg_user_data WHERE tg_id = %(tg_id)s;",
+                                   {'tg_id': str(update.message.from_user.id)})
+                    if not cursor.fetchall():
+                        cursor.execute(
+                            "INSERT INTO tg_user_data(tg_id, etis_login, etis_pass) VALUES (%(tg_id)s,%(etis_login)s,%(etis_pass)s);",
+                            {'tg_id': str(update.message.from_user.id),
+                             'etis_login': login_dict[update.message.from_user.id],
+                             'etis_pass': pass_dict[update.message.from_user.id]})
+                        cursor.execute('SELECT * FROM tg_user_data;')
+                    else:
+                        cursor.execute(
+                            "UPDATE tg_user_data SET etis_login = %(etis_login)s, etis_pass = %(etis_pass)s WHERE tg_id= %(tg_id)s;",
+                            {'tg_id': str(update.message.from_user.id),
+                             'etis_login': login_dict[update.message.from_user.id],
+                             'etis_pass': pass_dict[update.message.from_user.id]})
+                    conn.commit()
+            update.message.reply_text('Вход успешен. Для запуска работы бота нажмите /bot_start: ')
+            auth_dict[update.message.from_user.id] = True
+        else:
+            del session_dict[update.message.from_user.id]
+            update.message.reply_text('Неверный логин или пароль. Пожалуйста, повторите ввод /login. Для '
+                                      'просмотра введённых данных нажмите /user_data')
+            auth_dict[update.message.from_user.id] = False
 
 
 def text_handler(bot, update):
@@ -99,6 +137,7 @@ if __name__ == '__main__':
     updater.dispatcher.add_handler(CommandHandler("start", start_handler))
     updater.dispatcher.add_handler(CommandHandler("login", login_handler))
     updater.dispatcher.add_handler(CommandHandler("user_data", user_data_handler))
+    updater.dispatcher.add_handler(CommandHandler("authorize", auth_handler))
     updater.dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), text_handler))
 
     run(updater)
