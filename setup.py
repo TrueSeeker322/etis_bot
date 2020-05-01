d3b58f310e5ad414d6a27ca5cd750ffc14cf6980
True Seeker <murz.kirill@yandex.ru>
2020-05-01 12:52:57 +0500

    \fix\

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ def auth_handler(bot, update):
                             "INSERT INTO tg_user_data(tg_id, etis_login, etis_pass, auth, session_time) VALUES (%(tg_id)s,%(etis_login)s,%(etis_pass)s,%(auth)s, %(session_time)s);",
                             {'tg_id': str(update.message.from_user.id),
                              'etis_login': login_dict[update.message.from_user.id],
-                             'etis_pass': pass_dict[update.message.from_user.id],
+                             'etis_pass': pass_dict[update.message.from_user.id].decode('utf-8'),
                              'auth': 'True',
                              'session_time': str(time.time())})
                         cursor.execute('SELECT * FROM tg_user_data;')
@@ -83,7 +83,7 @@ def auth_handler(bot, update):
                             "UPDATE tg_user_data SET etis_login = %(etis_login)s, etis_pass = %(etis_pass)s, auth = %(auth)s, session_time = %(session_time)s WHERE tg_id= %(tg_id)s;",
                             {'tg_id': str(update.message.from_user.id),
                              'etis_login': login_dict[update.message.from_user.id],
-                             'etis_pass': pass_dict[update.message.from_user.id],
+                             'etis_pass': pass_dict[update.message.from_user.id].decode('utf-8'),
                              'auth': 'True',
                              'session_time': str(time.time())})
                     conn.commit()
